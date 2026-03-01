ebccbc9a12b7abec41c6c5af1cdb7c2a7791fa87
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-01 23:03:20 +0300

    Update helper_functions.py

--- datacenter/helper_functions.py
+++ datacenter/helper_functions.py
@@ -1,4 +1,5 @@
 import django
+from datetime import time
 from datetime import timedelta
 from django.utils.timezone import localtime
 
@@ -9,8 +10,12 @@ def get_duration(visit):
     return visit.leaved_at - visit.entered_at
 
 
-def format_duration(duration):    
-    return str(duration).split('.')[0]
+def format_duration(duration):
+	duration_seconds = int(duration.total_seconds())
+	hours = duration_seconds // 3600
+	minutes = (duration_seconds % 3600) // 60
+	seconds = (duration_seconds % 3600) % 60
+	return time(hours, minutes, seconds).strftime("%H:%M:%S")
 
 
 def is_visit_long(visit, minutes=60):
