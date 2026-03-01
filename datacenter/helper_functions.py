3564a5ce057e4e72e9499a079d96b438c46125ca
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-01 20:55:29 +0300

    Add files via upload

---
+++ datacenter/helper_functions.py
@@ -0,0 +1,19 @@
+import django
+from datetime import timedelta
+from django.utils.timezone import localtime
+
+
+def get_duration(visit):
+    if not visit.leaved_at:
+        return django.utils.timezone.now() - visit.entered_at
+    return visit.leaved_at - visit.entered_at
+
+
+def format_duration(duration):    
+    return str(duration).split('.')[0]
+
+
+def is_visit_long(visit, minutes=60):
+    if get_duration(visit) > timedelta(minutes=minutes):
+        return True
+    return False
